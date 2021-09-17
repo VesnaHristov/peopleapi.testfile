3fbeafbab71e65647919b17601928a12ea3299a4
Vesna <vesnahristov35@gmail.com>
2021-09-17 21:09:30 +0200

    initial commit

--- src/test/java/testFile.java
+++ src/test/java/testFile.java
@@ -9,14 +9,19 @@ public class testFile {
 PeopleAPIclient peopleAPIclient = new PeopleAPIclient();
 HttpResponse response;
 HttpResponse getpeople;
+HttpResponse getOnePerson;
+
 
    @Test
     public void  testFile() throws Exception {
        peopleAPIclient.getWellcomeReqwest();
-getpeople = peopleAPIclient.getAllPeople();
+       getpeople = peopleAPIclient.getAllPeople();
+       getOnePerson = peopleAPIclient.getOnePerson();
+
 
 String body = EntityUtils.toString(response.getEntity());
 String getAllPeople = EntityUtils.toString(getpeople.getEntity());
+String bodyofOnePerson = EntityUtils.toString(getOnePerson.getEntity());
 
         
 
