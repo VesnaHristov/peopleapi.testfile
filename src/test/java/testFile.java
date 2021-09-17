70f83614f061f22c12bf4ac1025dbed8320cfe1d
Vesna <vesnahristov35@gmail.com>
2021-09-17 19:24:02 +0200

    initial commit

--- src/test/java/testFile.java
+++ src/test/java/testFile.java
@@ -1,44 +1,15 @@
 
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
+import client.PeopleAPIclient;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.util.EntityUtils;
 import org.testng.annotations.Test;
 
-import javax.net.ssl.SSLContext;
-
 public class testFile {
-
-    @Test
+PeopleAPIclient peopleAPIclient = new PeopleAPIclient();
+HttpResponse response;
+   @Test
     public void  testFile() throws Exception {
-        Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE,"application/json");
-        SSLContext sslContext = SSLContextBuilder
-                .create()
-                .loadTrustMaterial(new TrustSelfSignedStrategy())
-                .build();
-
-        HttpGet request = new HttpGet("https://people-api1.herokuapp.com/");
-request.setHeader(contentType);
-
-        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
-
-        HttpResponse response = httpClient.execute(request);
-
-        HttpEntity entity = response.getEntity();
-        String body = EntityUtils.toString(response.getEntity());
-
-        HttpEntity newEntity = new StringEntity(body, ContentType.get(entity));
-        response.setEntity(newEntity);
+       peopleAPIclient.getWellcomeReqwest();
 
         
 
