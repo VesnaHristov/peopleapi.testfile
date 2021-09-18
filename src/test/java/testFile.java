d34e161aa8d3de8e375d71f61f6ac83092b090c7
Vesna <vesnahristov35@gmail.com>
2021-09-18 19:59:55 +0200

    initial commit day2

--- src/test/java/testFile.java
+++ src/test/java/testFile.java
@@ -10,13 +10,15 @@ PeopleAPIclient peopleAPIclient = new PeopleAPIclient();
 HttpResponse response;
 HttpResponse getpeople;
 HttpResponse getOnePerson;
+HttpResponse deleteOnePerson;
 
     @Test
     public void  testFile() throws Exception {
-       peopleAPIclient.deleteOnePerson();
+       peopleAPIclient.getWellcomeReqwest();
         getpeople = peopleAPIclient.getAllPeople();
         getOnePerson = peopleAPIclient.getOnePerson();
         peopleAPIclient.postOnePerson();
+        deleteOnePerson = peopleAPIclient.deleteOnePerson();
 
         String body = EntityUtils.toString(response.getEntity());
         String getAllPeople = EntityUtils.toString(getpeople.getEntity());
