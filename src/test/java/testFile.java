8585191a45477cebb8645eec072785a6715f5393
Vesna <vesnahristov35@gmail.com>
2021-09-19 19:40:27 +0200

    initial commit day3

--- src/test/java/testFile.java
+++ src/test/java/testFile.java
@@ -8,23 +8,12 @@ import org.testng.annotations.Test;
 public class testFile {
 PeopleAPIclient peopleAPIclient = new PeopleAPIclient();
 HttpResponse response;
-HttpResponse getpeople;
-HttpResponse getOnePerson;
-HttpResponse deleteOnePerson;
-HttpResponse putPerson;
 
     @Test
     public void  testFile() throws Exception {
-       peopleAPIclient.getWellcomeReqwest();
-        getpeople = peopleAPIclient.getAllPeople();
-        getOnePerson = peopleAPIclient.getOnePerson();
-        peopleAPIclient.postOnePerson();
-        deleteOnePerson = peopleAPIclient.deleteOnePerson();
-        putPerson = peopleAPIclient.putPerson();
+    //    response = peopleAPIclient.postOnePerson();
 
-        String body = EntityUtils.toString(response.getEntity());
-        String getAllPeople = EntityUtils.toString(getpeople.getEntity());
-        String bodyofOnePerson = EntityUtils.toString(getOnePerson.getEntity());
+      //  String body = EntityUtils.toString(response.getEntity());
+    }
 
-  }
 }
